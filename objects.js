8e5d21ab3f2668d5c2775ddfb91eca652acbf86a
Stephen Castaneda <stephen.castaneda40@gmail.com>
2020-01-20 19:39:43 -0600

    added objects

---
+++ objects.js
@@ -0,0 +1,21 @@
+console.log('objects')
+
+const wordLength = (word) => {
+    return word.length;
+
+
+
+
+console.log(wordLength('cat');
+console.log(wordLength('monkeybutt');
+
+
+const ageCheck = (age) => {
+     if(age >= 21){
+         console.log("Party!!");
+     } else {
+   console.log('no drinks for you')
+     }
+}
+ageCheck(12);
+ageCheck(24);
