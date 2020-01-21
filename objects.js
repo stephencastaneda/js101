5ed04d1a1356e795180bef85dec4de811507b756
Stephen Castaneda <stephen.castaneda40@gmail.com>
2020-01-20 20:44:11 -0600

    created objects

--- objects.js
+++ objects.js
@@ -2,12 +2,12 @@ console.log('objects')
 
 const wordLength = (word) => {
     return word.length;
+}
 
 
 
-
-console.log(wordLength('cat');
-console.log(wordLength('monkeybutt');
+console.log(wordLength('cat'));
+console.log(wordLength('monkeybutt'));
 
 
 const ageCheck = (age) => {
@@ -19,3 +19,36 @@ const ageCheck = (age) => {
 }
 ageCheck(12);
 ageCheck(24);
+
+const expense = {
+    dateCreated: '01/17/2020',
+    location: 'Baja Burrito',
+    cost: 12.57, 
+    isGood: true
+}
+//dot notation
+console.log(expense.cost);
+
+//bracket notation 
+console.log(expense['cost'])
+
+const employee = {
+    dateHired: '01/17/2020',
+    from: 'Nashville',
+    collegeDegree: true,
+    hourlyWage: 15.50,
+    firstName: 'Stephen',
+    lastName: 'Castaneda',
+}
+
+const statusMaker = (emp)=> {
+   if (emp.firstName[0] === 'M') {
+       emp.status = 'vip';  
+
+   } else {
+      emp.status = 'peasant';
+       
+   }
+   return emp;
+}
+console.log(statusMaker(employee));
